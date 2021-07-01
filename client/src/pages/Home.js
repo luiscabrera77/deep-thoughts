75dbaa8b4adc8ae750010112bb4afd238ef1ff99
Luis <luis.cabrera@gmail.com>
2021-07-01 19:42:37 -0400

    Heroku build ready

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
-import FriendList from '../components/FriendList';
-import Auth from '../utils/auth';
 import ThoughtForm from '../components/ThoughtForm';
+import FriendList from '../components/FriendList';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 
 
